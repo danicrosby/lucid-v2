ce42f510df6a2f9c0425c88cece11f106f6b23ef
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-22 18:40:13 -0600

    refactored tagging page, added dropdowns and styling

---
+++ Tag/tag.js
@@ -0,0 +1,9 @@
+console.log("hello from tag.js")
+
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "50px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
