74f90267b9e01ccb8ddf68c98ec1944d83e0b267
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-25 15:42:56 -0600

    more tweaks to animations

--- main.js
+++ main.js
@@ -2,8 +2,3 @@ console.log("Greetings, Beautiful, from main.js")
 
 //HEART ANIMATION FUNCTION
 
-$(function() {
-  $(".HeartAnimation").click(function() {
-    $(this).toggleClass("animate");
-  });
-});
