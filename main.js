c5ef2a70e95477ebbdf220cbfa98b8d34d664228
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-25 15:31:49 -0600

    tweaks to cloud animation welcome page

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 console.log("Greetings, Beautiful, from main.js")
 
+//HEART ANIMATION FUNCTION
+
+$(function() {
+  $(".HeartAnimation").click(function() {
+    $(this).toggleClass("animate");
+  });
+});
